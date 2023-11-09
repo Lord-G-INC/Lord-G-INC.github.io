17f99ff7dbbe4fda86e328a9ad78734326f9f0c2
nico-apps <nicomarikucza@googlemail.com>
2023-11-09 19:24:47 +0100

    Dynamically load in page content

--- galaxy-hack-database-new/database.js
+++ galaxy-hack-database-new/database.js
@@ -1,14 +1,33 @@
-let databaseJson = [];
+let modDatabase = [];
+
+function findEntry (entryToFind) {
+    return modDatabase.find((currentEntry) => currentEntry["modName"] == entryToFind);
+}
 function fetchDatabase () {
     $.ajax({
         type: "GET",
         url: "database.json",
         success: function (response) {
-            databaseJson = JSON.parse(response);
+            modDatabase = JSON.parse(response);
             requestPage(localStorage.getItem("requestedPage"));
         }
     });
 }
 function requestPage (pageToGet) {
-    
+    const page = findEntry(pageToGet);
+    var galleryHtml = "";
+
+    $("#modName").text(page.modName);
+    $("#modRatings").text(page.ratingInfo);
+    $("#modLogo").attr("src", page.logoSrc);
+    // Formatting through HTML allowed
+    $("#modDescription").html(page.description);
+    $("#modInformation").html(page.information);
+    $("#modDownload").html("<a href=\"" + page.downloadUrl + "\">" + page.versionInfo + "</a>");
+    $("#modCredits").html(page.creditInfo);
+    $("#modGalleryText").html(page.galleryInfo);
+
+    for (currentImg of page.galleryImages) 
+        galleryHtml += "<img src=\"" + currentImg + "\">";
+    $("#modGalleryContainer").html(galleryHtml);
 }
